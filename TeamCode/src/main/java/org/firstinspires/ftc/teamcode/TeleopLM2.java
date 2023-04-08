e0df741c93fcbff1ead202d903715827a6aef4d5
Vikram Kommera <outcastvirus24@icloud.com>
2023-04-08 16:09:00 -0700

    Update Teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopLM2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopLM2.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,7 +10,7 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
-
+@Disabled
 @TeleOp(name = "Teleop For League Tournament", group = "16481-Power-Play")
 public class TeleopLM2 extends LinearOpMode {
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopUpdateA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopUpdateA.java
@@ -11,7 +11,13 @@ import org.firstinspires.ftc.teamcode.gaeldrive.AlphaUpdate;
 @TeleOp(name = "Teleop For Testing Alpha Update", group = "16481-Power-Play")
 public class TeleopUpdateA extends LinearOpMode {
 
-    ElapsedTime loopTime = new ElapsedTime();
+    ElapsedTime loopTime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+
+    Pose2d robotPoseEstimate = new Pose2d(0,0,0);
+
+    double botX = 0;
+    double botY = 0;
+    double botHeading = 0;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -20,26 +26,38 @@ public class TeleopUpdateA extends LinearOpMode {
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         drive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        drive.setPoseEstimate(robotPoseEstimate);
         while (opModeInInit()) {
-            
+
         }
 
         while (!isStopRequested()) {
+            loopTime.reset();
+
             drive.setWeightedDrivePower(new Pose2d(-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x));
             drive.update();
 
-            Pose2d robotPoseEstimate = AlphaUpdate.mecanumPoseDeltaCalc(drive, gamepad1, loopTime);
+            Pose2d PoseDelta = AlphaUpdate.mecanumPoseDeltaCalc(drive, gamepad1, loopTime);
 
-            loopTime.reset();
+            botX = botX + PoseDelta.getX();
+            botY = botY + PoseDelta.getY();
+            botHeading = botHeading + PoseDelta.getHeading();
+
 
             // Telemetry
-            telemetry.addData("Roboracers Teleop for Testing Update A", "");
+            telemetry.addData("RoboRacers Teleop for Testing Update A", "");
             telemetry.addData("loopTime Resolution", loopTime.getResolution());
-            telemetry.addData("x", robotPoseEstimate.getX());
-            telemetry.addData("y", robotPoseEstimate.getY());
-            telemetry.addData("heading", robotPoseEstimate.getHeading());
+            Pose2d accuratePoseEstimate = drive.getPoseEstimate();
+            telemetry.addData("accurate x", accuratePoseEstimate.getX());
+            telemetry.addData("accurate y", accuratePoseEstimate.getY());
+            telemetry.addData("accurate heading", accuratePoseEstimate.getHeading());
+
+            telemetry.addData("estimated x", botX);
+            telemetry.addData("estimated y", botY);
+            telemetry.addData("estimated heading", botHeading);
             telemetry.update();
 
+            loopTime.reset();
         }
     }
 
