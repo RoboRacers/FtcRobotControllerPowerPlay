e9045e1b0c4eed259988cedbc8990288fcb50056
SKS-Student <67933648+SKS-Student@users.noreply.github.com>
2023-07-28 20:26:00 -0700

    File for converting Power Play TeleOp into State Machines (Added Template #2 - Incomplete)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StateMachinesTeleopLM2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StateMachinesTeleopLM2.java
@@ -9,9 +9,10 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
-/*
+
 @TeleOp(name = "Teleop For League Tournament", group = "16481-Power-Play")
 public class StateMachinesTeleopLM2 extends LinearOpMode {
+
     /*
  //Turning power play teleOp into state machines
      public enum STATE_CLAW {
@@ -56,3 +57,45 @@ public class StateMachinesTeleopLM2 extends LinearOpMode {
         }
      */
 
+    //Turning power play teleOp into state machines
+    public enum STATE_CLAW {
+        STATE_CLAW_OPEN,
+        STATE_CLAW_CLOSE
+    }
+    public enum STATE_ARM {
+        STATE_ARM_LOW,
+        STATE_ARM_MED,
+        STATE_ARM_HIGH,
+        STATE_ARM_MANUAL_UP,
+        STATE_ARM_MANUAL_DOWN
+    }
+
+    public enum STATE_ARM {
+        STATE_ARM_LOW,
+        STATE_ARM_MED,
+        STATE_ARM_HIGH,
+        STATE_ARM_MANUAL_UP,
+        STATE_ARM_MANUAL_DOWN
+    }
+
+    //Setting Current state for each section to desired starting state
+    public STATE_CLAW InitCLAW = STATE_CLAW.STATE_CLAW_OPEN;
+
+     while runOpMode{
+        switch (InitCLAW) {
+            case (if gamepad2.x):
+                InitCLAW = StateMachinesTeleopLM2.STATE_CLAW.STATE_CLAW_CLOSE;
+                break;
+        }
+        switch (STATE_CLAW.STATE_CLAW_OPEN) {
+            case (if gamepad2.x):
+                InitCLAW = StateMachinesTeleopLM2.STATE_CLAW.STATE_CLAW_CLOSE;
+                break;
+        }
+        switch (STATE_CLAW.STATE_CLAW_CLOSE) {
+            case (if gamepad2.y):
+                InitCLAW = StateMachinesTeleopLM2.STATE_CLAW.STATE_CLAW_OPEN;
+                break;
+        }
+    }
+}
